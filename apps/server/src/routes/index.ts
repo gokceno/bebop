c36e4532478145e5fa654bd1fa3aac264a89dff4
Gokcen Ogutcu <gokcen@brewww.com>
2025-05-25 00:36:25 +0300

    chore: Remove file

--- apps/server/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import type { FastifyInstance, FastifyPluginAsync } from 'fastify';
-
-// This file enables proper loading of route plugins
-const routeIndex: FastifyPluginAsync = async (fastify: FastifyInstance) => {
-  // This function is intentionally empty
-  // It allows the autoload plugin to load this directory correctly
-};
-
-export default routeIndex;
